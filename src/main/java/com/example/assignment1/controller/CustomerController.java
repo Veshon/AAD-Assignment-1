485e61e180e74fb44c08ba53eab68e8fe36f76a6
veshon <vashonmabima12@gmail.com>
2024-08-18 06:34:49 +0530

    GET

--- src/main/java/com/example/assignment1/controller/CustomerController.java
+++ src/main/java/com/example/assignment1/controller/CustomerController.java
@@ -76,7 +76,7 @@ public class CustomerController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("Hey");
     }
 
     @Override
